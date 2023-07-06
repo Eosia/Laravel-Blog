571d4b261897d51a979b441809389d7ea15d5908
Eosia <kevin.eosiaweb@gmail.com>
2023-07-06 21:52:58 +0200

    first commit

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,9 @@
+
+
+    <div class="my-4">Catégories</div>
+    <div class="list-group">
+        <a href="#" class="list-group-item active">Laravel</a>
+        <a href="#" class="list-group-item">PHP</a>
+        <a href="#" class="list-group-item">Javascript</a>
+    </div>
+
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    UserController,
+    ArticleController
+};
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('layouts.main');
+});
+
+Route::get('profile/{username}', [UserController::class, 'profile'])
+    ->name('user.profile');
+
+Route::resource('article', ArticleController::class);
+
+Route::get('test', function () {
+    return view('test');
+});
+
+Route::get('structures', function () {
+   return view('structures');
+});
