ace83586d7c8bc685447786cbd0fce2b0b21508e
Eosia <kevin.eosiaweb@gmail.com>
2023-10-14 17:45:25 +0200

    ajout transaction profil user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules\Unique;
 use App\Models\User;
-use Storage, Image, Str;
+use Nette\Schema\ValidationException;
+use Storage, Image, Str, DB;
 
 class UserController extends Controller
 {
@@ -36,41 +37,51 @@ class UserController extends Controller
     public function store()
     {
 
-        $user = auth()->user();
+        DB::beginTransaction();
 
-        $user = $user->updateOrCreate(['id' => $user->id],
-            request()->validate([
-                'name' => ['required', 'min:3', 'max:20', Rule::unique('users')->ignore($user)],
-                'email' => ['required', 'email', Rule::unique('users')->ignore($user)],
-                'avatar' => ['sometimes', 'nullable', 'file', 'image', 'mimes:jpeg,png', 'dimensions:min_width=200,min_height=200'],
-            ]));
+        try {
+            $user = auth()->user();
+            $user = $user->updateOrCreate(['id' => $user->id],
+                request()->validate([
+                    'name' => ['required', 'min:3', 'max:20', Rule::unique('users')->ignore($user)],
+                    'email' => ['required', 'email', Rule::unique('users')->ignore($user)],
+                    'avatar' => ['sometimes', 'nullable', 'file', 'image', 'mimes:jpeg,png', 'dimensions:min_width=200,min_height=200'],
+                ]));
 
-        if (request()->hasFile('avatar') && request()->file('avatar')->isValid()) {
+            if (request()->hasFile('avatar') && request()->file('avatar')->isValid()) {
 
-            if(Storage::exists('avatar/' . $user->id)) {
-                Storage::deleteDirectory('avatars/' . $user->id);
+                if (Storage::exists('avatar/' . $user->id)) {
+                    Storage::deleteDirectory('avatars/' . $user->id);
+                }
+
+                $ext = request()->file('avatar')->extension();
+                $filename = Str::slug($user->name) . '-' . $user->id . '.' . $ext;
+
+                $path = request()->file('avatar')->storeAs('avatars/' . $user->id, $filename);
+
+                $thumbnailImage = Image::make(request()->file('avatar'))->fit(200, 200, function ($constraint) {
+                    $constraint->upsize();
+                })->encode($ext, 50);
+
+                $thumbnailPath = 'avatars/' . $user->id . '/tumbnail/' . $filename;
+
+                Storage::put($thumbnailPath, $thumbnailImage);
+                $user->avatar()->updateOrCreate(['user_id' => $user->id], [
+                    'filename' => $filename,
+                    'url' => Storage::url($path),
+                    'path' => $path,
+                    'thumb_url' => Storage::url($thumbnailPath),
+                    'thumb_path' => $thumbnailPath
+                ]);
             }
-
-            $ext = request()->file('avatar')->extension();
-            $filename = Str::slug($user->name) . '-' . $user->id . '.' . $ext;
-
-            $path = request()->file('avatar')->storeAs('avatars/' . $user->id, $filename);
-
-            $thumbnailImage = Image::make(request()->file('avatar'))->fit(200, 200, function ($constraint) {
-                $constraint->upsize();
-            })->encode($ext, 50);
-
-            $thumbnailPath = 'avatars/' . $user->id . '/tumbnail/' . $filename;
-
-            Storage::put($thumbnailPath, $thumbnailImage);
-            $user->avatar()->updateOrCreate(['user_id' => $user->id], [
-                'filename' => $filename,
-                'url' => Storage::url($path),
-                'path' => $path,
-                'thumb_url' => Storage::url($thumbnailPath),
-                'thumb_path' => $thumbnailPath
-            ]);
         }
+
+        catch(ValidationException $e) {
+            DB::rollback();
+            dd($e->getErrors());
+        }
+
+        DB::commit();
 
         $success = 'Informations mises à jour';
         return back()->withSuccess($success);
